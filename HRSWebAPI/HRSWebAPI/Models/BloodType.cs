d8e5011d9cc89ed2d8653749e9debd7cce165d1b
Richard Verdera, Jr <rverdera@pcso.gov.ph>
2022-02-11 23:14:53 +0800

    Add DbContext

---
+++ HRSWebAPI/HRSWebAPI/Models/BloodType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRSWebAPI.Models
+{
+    public partial class BloodType
+    {
+        public BloodType()
+        {
+            PersonalInformations = new HashSet<PersonalInformation>();
+        }
+
+        public int BloodTypeID { get; set; }
+        public string? BloodTypeDesc { get; set; }
+        public bool? IsDel { get; set; }
+
+        public virtual ICollection<PersonalInformation> PersonalInformations { get; set; }
+    }
+}
---
+++ HRSWebAPI/HRSWebAPI/Models/CivilStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRSWebAPI.Models
+{
+    public partial class CivilStatus
+    {
+        public CivilStatus()
+        {
+            PersonalInformations = new HashSet<PersonalInformation>();
+        }
+
+        public int CivilStatusID { get; set; }
+        public string? CivilStatusDesc { get; set; }
+        public bool? IsDel { get; set; }
+
+        public virtual ICollection<PersonalInformation> PersonalInformations { get; set; }
+    }
+}
